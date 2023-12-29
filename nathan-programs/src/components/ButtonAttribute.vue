9a169e473c0850598a7c42f8eeec0b32633e26fa
NathanSuss <n8sussman@gmail.com>
2023-12-29 04:16:40 -0600

    34:37 into vue crash course

---
+++ nathan-programs/src/components/ButtonAttribute.vue
@@ -0,0 +1,14 @@
+<template>
+    <button v-bind:style="{background: color}" class="btn">{{text}}</button>
+</template>
+
+<script>
+export default {
+    name: "ButtonAttribute",
+    props: {
+        text: String,
+        color: String
+    }
+}
+
+</script>
---
+++ nathan-programs/src/components/HeaderAttribute.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <ButtonAttribute text="Add Task" color="green" ></ButtonAttribute>
+  </header>
+</template>
+
+<script>
+import ButtonAttribute from './ButtonAttribute.vue';
+
+
+export default {
+  name: 'HeaderAttribute',
+  // make it so HeaderAttribute attributes want a title variable to be specified
+  // and print a default string when none is
+  props: {
+    title: {
+      type: String,
+      default: '\'title: String\' not provided to header attribute'
+    }
+  },
+  components: { ButtonAttribute }
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
